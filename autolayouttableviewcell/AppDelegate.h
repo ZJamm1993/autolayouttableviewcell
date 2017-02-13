52474d92be91627dc3ba8cd83cd53e3fd2715639
LX <sb@gmail.com>
2017-02-13 09:54:16 +0800

    Initial Commit

---
+++ autolayouttableviewcell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  autolayouttableviewcell
+//
+//  Created by iMac206 on 17/2/13.
+//  Copyright © 2017年 jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ autolayouttableviewcell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  autolayouttableviewcell
+//
+//  Created by iMac206 on 17/2/13.
+//  Copyright © 2017年 jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
